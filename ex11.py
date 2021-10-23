30d5c124c63252d20fe8ebf31b3504349fe83b4d
Zane Bartlett <zanebartlet1@gmail.com>
2021-10-23 14:14:20 -0500

    add ex11, ex12

---
+++ ex11.py
@@ -0,0 +1,6 @@
+print("How many cocks can you garggle wessley", end=' ')
+cg = input()
+print("Computer, how many cocks can fit in wessley's mouth if everyone on the deck was forced to try", end=' ')
+dt = input()
+
+print(f"Captain, I don't know that I want to {cg} any cock, besides, you're the only person on the deck with a....cock.\n\"Comptuer, give everyone else on the deck cocks, and a reason to try. A good reason. Now Computer, how many cocks can wessley fit in his mouth\"\nComputer\"{dt}")
