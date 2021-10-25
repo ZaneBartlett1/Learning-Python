e49cc4dfa267934183f2396e0e9771e3f6032ce5
Zane Bartlett <zanebartlet1@gmail.com>
2021-10-24 22:01:03 -0500

    ex15_2files

---
+++ ex15/ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+filename, script = argv
+
+txt = open(script)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
