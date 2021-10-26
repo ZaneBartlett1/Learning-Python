c4f39d9b6457be51d1c7053b3fb0b0f1632bff21
Zane Bartlett <zanebartlet1@gmail.com>
2021-10-25 19:46:19 -0500

    ex16

---
+++ ex16/ex16.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to earse {filename}.")
+print("If you don't want to do that, hit CTRL -C (^C).")
+print("If you do want to do that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("Line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+
+print("And finally, we close it.")
+target.close()
